90421655ef10d89dfd8ecba2b3d660b37d776c9c
yesongjoo <yesong.joo7@gmail.com>
2023-12-29 16:35:48 -0500

    main page hero image

--- eastyorkgardenclub.client/src/mainPage/SectionHero.jsx
+++ eastyorkgardenclub.client/src/mainPage/SectionHero.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import "/src/css/general.css";
 import "/src/css/mainPage/index.css";
+import "/src/css/mainPage/MainQueries.css";
 
 class SectionHero extends Component {
   constructor(props) {
@@ -13,24 +14,30 @@ class SectionHero extends Component {
     return (
       <section className="section-hero">
         <div className="hero">
-          <div className="hero-img-box">
-            <img
-              className="hero-img"
-              src="src/img/gardening-main.jpg"
-              alt="people are gardening together"
-            />
-          </div>
-          <div className="hero-text-box">
-            <h1 className="heading-primary hero-heading">
-              <span>Hello! Welcome to the</span> East York Garden Club
-            </h1>
-            <p className="hero-description">
-              {"Let's make it green together. Let's be happy together."} <br />
-              {"Let's make it green together."}
-            </p>
-            <Link className="learn-more-link" onClick={() => navigate("about")}>
-              Learn more &gt;
-            </Link>
+          <div className="hero-img"></div>
+          <div className="hero-container">
+            <div className="hero-content-box">
+              <div className="hero-content">
+                <h1 className="heading-primary hero-heading">
+                  <span>
+                    Hello! Welcome to <br /> the
+                  </span>{" "}
+                  East York <br />
+                  Garden Club
+                </h1>
+                <p className="hero-description">
+                  {"Let's make it green together. Let's be happy together."}{" "}
+                  <br />
+                  {"Let's make it green together."}
+                </p>
+                <Link
+                  className="learn-more-link"
+                  onClick={() => navigate("about")}
+                >
+                  Learn more &gt;
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
       </section>
